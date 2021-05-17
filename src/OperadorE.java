c2f6fa35278b87e67b28810af9cb9ea28e54d20e
Beatriz Cardoso Cunha <beacarcun@gmail.com>
2021-05-17 15:36:26 -0300

    Implementado a estrutura Switch

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -13,7 +13,7 @@ public class OperadorE {
 		senha = leitor.next();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
 		
-		if (usuario.equals("administrador") && senha.equals("123")) {
+		if (usuario.equalsIgnoreCase("administrador") && senha.equals("123")) {
 			System.out.println("Login realizado com sucesso!");
 		}else {
 			System.out.println("Usuário ou senha incorretos");
